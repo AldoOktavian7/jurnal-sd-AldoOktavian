14571b6e33230d33299cd7dbbccbddbd9b0ddcd6
Aldo Oktavian <0240_aldookta@mhs.unp.ac.id>
2026-05-22 18:17:17 +0700

    [FIX] Menambahkan Penanganan error pada proses integritas data.

--- belajar-git/project-mvc/controllers/api_handler.py
+++ belajar-git/project-mvc/controllers/api_handler.py
@@ -1,5 +1,21 @@
+import random
+
 # Simulasikan data dari database
-users = [{"id": 1, "name": "Admin"}, {"id": 2, "name": "User"}]
+users = [
+    {"id": 1, "name": "Admin"},
+    {"id": 2, "name": "User"}
+]
 
 def get_users():
-    return {"status": "success", "data": users}
+    angka = random.randint(1, 5)
+
+    if angka == 3:
+        return {
+            "status": "error",
+            "message": "Server sedang sibuk, coba lagi nanti."
+        }
+
+    return {
+        "status": "success",
+        "data": users
+    }
--- belajar-git/project-mvc/views/dashboard_component.py
+++ belajar-git/project-mvc/views/dashboard_component.py
@@ -1,11 +1,21 @@
 def fetch_data_from_api(api_function):
     print("[System] Mencoba menghubungkan ke API...")
+    
     try:
         response = api_function()
+
         if response["status"] == "success":
+            print("[Success] Data berhasil diambil dari API")
             return response["data"]
+
         else:
-            raise Exception("API Return Error")
+            error_message = response.get(
+                "message",
+                "Terjadi kesalahan pada server."
+            )
+
+            raise Exception(error_message)
+
     except Exception as e:
         print(f"[Error] Gagal Integrasi: {e}")
         return None
