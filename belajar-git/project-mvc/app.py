2667bca585774c7f48a02283272c0edc076bb83d
Aldo Oktavian <0240_aldookta@mhs.unp.ac.id>
2026-06-10 17:40:02 +0700

    [FEAT] Implementasi Named Volume dan Docker Compose Minggu 13

--- belajar-git/project-mvc/app.py
+++ belajar-git/project-mvc/app.py
@@ -1,9 +1,14 @@
 import os
+import time
 
-# Membaca variabel lingkungan dengan nilai fallback/default
 user_name = os.getenv('APP_USER', 'Guest')
 app_env = os.getenv('APP_ENV', 'development')
 
 if __name__ == "__main__":
     print(f"Halo {user_name}! Aplikasi ini berjalan di dalam kontainer Docker.")
     print(f"Environment saat ini: {app_env}")
+    
+    print("Kontainer standby. Menunggu perintah...")
+    # Loop ini menjaga agar kontainer tetap hidup terus di latar belakang
+    while True:
+        time.sleep(3600)
